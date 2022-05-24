691d3df493bcb4792e2b418580cb92a9cb7198b3
takashithiba <k021g1120@g.neec.ac.jp>
2022-05-24 13:07:54 +0900

    5/17

---
+++ Assets/GameManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class GameManager : MonoBehaviour
+{
+    public void SceneReset()
+    {
+        string activeSceneName = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(activeSceneName);
+    }
+
+    public void ChangeScene(string nextScene)
+    {
+        SceneManager.LoadScene(nextScene);
+    }
+}
